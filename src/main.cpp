5180da34385fdc97917fe4286ad264018b4020de
TheBoJeu <TheBoje@users.noreply.github.com>
2022-01-21 14:09:09 -0500

    add nearly empty OpenFrameworks project

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,14 @@
 #include <iostream>
+#include "ofMain.h"
+#include "application.h"
 
-#include <GLFW/glfw3.h>
+int main(int argc, char**argv) {
+	ofGLFWWindowSettings windowSettings;
 
-int main(int argc, char * argv[]) {
-    GLFWwindow* window;
+	ofSetupOpenGL(512, 512, OF_WINDOW);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+  	// instancier et lancer l'application (openFrameworks 0.11.0)
+  	ofRunApp(new Application());
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // OpenGL section here
-        {
-            // Draw a triangle in the middle of the window to check if OpenGL works
-            glBegin(GL_TRIANGLES);
-            glVertex2f(-0.5f, 0.0f);
-            glVertex2f( 0.0f, 0.5f);
-            glVertex2f( 0.0f, 0.0f);
-            glEnd();
-        }
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
