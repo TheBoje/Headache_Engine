5b34b9954aff9da8fb968f1a8c975ecc08ee3180
TheBoje <louis.leenar@gmail.com>
2022-02-07 01:20:57 -0500

    add ofImage convertion from any colorspace to any colorspace (eg. RGB -> HSB) using openCV & ofxCv (requires to download addon at https://github.com/kylemcdonald/ofxCv)

--- src/ImageUtils.cpp
+++ src/ImageUtils.cpp
@@ -1,46 +1,61 @@
 #include "ImageUtils.h"
 
-namespace ift3100 {
-    ofImage * ImageUtils::importImage(const std::string& path) {
-        ofLog() << "<utils:import> from " << path;
-        return new ofImage(path);
-    }
 
-    void ImageUtils::exportImage(const std::string& name) {
-        ofLog() << "<utils:export> " << name
-                << " (" << ofGetWindowWidth() 
-                << ", " << ofGetWindowHeight()  << ")";
-        ofImage res;
-        res.grabScreen(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
-        res.save(name);
-    }
+ofImage * ift3100::ImageUtils::importImage(const std::string& path) {
+    ofLog() << "<utils:import> from " << path;
+    return new ofImage(path);
+}
 
-    /**
-     * Compute the histogram of the three colors of the image.
-     * Allocate a 2 dimensional array and give the use the property, it's up to you
-     * to delete it.
-     *
-     * @param image
-     * @param space
-     * @return a 3 lengthed array of 256 lengthed unsigned int vectors for rbg colors
-     */
-    unsigned int ** ImageUtils::computeHistRGB(const ofImage &image) {
+void ift3100::ImageUtils::exportImage(const std::string& name) {
+    ofLog() << "<utils:export> " << name
+            << " (" << ofGetWindowWidth() 
+            << ", " << ofGetWindowHeight()  << ")";
+    ofImage res;
+    res.grabScreen(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
+    res.save(name);
+}
 
-        int channels = 3;
-        unsigned int ** hist = new unsigned int * [channels];
+/**
+ * Compute the histogram of the three colors of the image.
+ * Allocate a 2 dimensional array and give the use the property, it's up to you
+ * to delete it.
+ *
+ * @param image
+ * @param space
+ * @return a 3 lengthed array of 256 lengthed unsigned int vectors for rbg colors
+ */
+unsigned int ** ift3100::ImageUtils::computeHistRGB(const ofImage &image) {
 
-        for(int i = 0; i < channels; i++)
-            hist[i] = new unsigned int[256]();
+    int channels = 3;
+    unsigned int ** hist = new unsigned int * [channels];
 
-        for(int i = 0; i < (int)image.getWidth(); i++) {
-            for(int j = 0; j < (int)image.getHeight(); j++) {
-                ofColor color = image.getColor(i, j);
-                hist[0][color.r]++;
-                hist[1][color.g]++;
-                hist[2][color.b]++;
-            }
+    for(int i = 0; i < channels; i++)
+        hist[i] = new unsigned int[256]();
+
+    for(int i = 0; i < (int)image.getWidth(); i++) {
+        for(int j = 0; j < (int)image.getHeight(); j++) {
+            ofColor color = image.getColor(i, j);
+            hist[0][color.r]++;
+            hist[1][color.g]++;
+            hist[2][color.b]++;
         }
-
-        return hist;
     }
+
+    return hist;
+}
+
+/**
+ * Convert an image using openCV library.
+ * 
+ * @param image 
+ * @param code convertion code, see https://docs.opencv.org/3.1.0/de/d25/imgproc_color_conversions.html
+ * @return image converted following `code` type.
+ */
+ofImage ift3100::ImageUtils::convert(const ofImage& image, int code) {
+    cv::Mat tmp = ofxCv::toCv(image); // Convert from ofImage to cv::Mat (openCV image object)
+    cv::Mat colored;
+    cv::cvtColor(tmp, colored, code); // Convert color 
+    ofImage res;
+    ofxCv::toOf(colored, res);        // Convert from cv::Mat to ofImage 
+    return res;
 }
