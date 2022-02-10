85d958441b483da6e47d0ec4836f863f97f91857
Altimors <vincent.commin2000@orange.fr>
2022-02-10 10:40:22 -0500

    added setter to properties

--- src/DrawingProperties.h
+++ src/DrawingProperties.h
@@ -3,6 +3,7 @@
 
 #include "ofColor.h"
 #include "Constant.h"
+#include "ofxGui.h"
 
 namespace ift3100 {
 
@@ -17,6 +18,29 @@ namespace ift3100 {
         ofColor FILLED_COLOR = DEFAULT_FILLED_COLOR;
         
         float STROKE_WIDTH = DEFAULT_STROKE_WIDTH;
+
+        void setStrokeColor(int r, int g, int b, int a) {
+            STROKE_COLOR.r = r;
+            STROKE_COLOR.g = g;
+            STROKE_COLOR.b = b;
+            STROKE_COLOR.a = a;
+        }
+
+        void setStrokeColor(ofColor color) {
+            STROKE_COLOR = color;
+        }
+
+        void setFilledColor(int r, int g, int b, int a) {
+            FILLED_COLOR.r = r;
+            FILLED_COLOR.g = g;
+            FILLED_COLOR.b = b;
+            FILLED_COLOR.a = a;
+        }
+
+        void setFilledColor(ofColor color) {
+            FILLED_COLOR = color;
+        }
+        
     };
 
 }
