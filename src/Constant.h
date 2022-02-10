100256cf93231e6b812e163ff95c933e0f792e11
Altimors <vincent.commin2000@orange.fr>
2022-02-10 10:42:57 -0500

    change constant values

--- src/Constant.h
+++ src/Constant.h
@@ -5,10 +5,10 @@
 
 namespace ift3100 {
 
-    constexpr ofColor DEFAULT_BACKGROUND_COLOR(255, 255, 255);
+    constexpr ofColor DEFAULT_BACKGROUND_COLOR(ofColor::white);
 
-    constexpr ofColor DEFAULT_STROKE_COLOR(0, 0, 0);
-    constexpr ofColor DEFAULT_FILLED_COLOR(0, 0, 0);
+    constexpr ofColor DEFAULT_STROKE_COLOR(ofColor::black);
+    constexpr ofColor DEFAULT_FILLED_COLOR(ofColor::black);
 
     constexpr float DEFAULT_STROKE_WIDTH = 10.0f;
 }
