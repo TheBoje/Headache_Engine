7fbb31d2988554fcc34767148944cbe690f82343
TheBoje <louis.leenar@gmail.com>
2022-02-23 00:18:56 -0500

    add: override Hierarchy map method for Hierarchy<InterfaceItem> obj to draw each obj

--- src/InterfaceItem.hpp
+++ src/InterfaceItem.hpp
@@ -1,6 +1,7 @@
 #ifndef INTERFACE_ITEM_HPP
 #define INTERFACE_ITEM_HPP
 
+#include "Hierarchy.h"
 namespace ift3100 {
     /**
      * This class is an interface to mondel onto to add an UI element.
@@ -16,6 +17,17 @@ namespace ift3100 {
             */
             virtual void draw() = 0;
     };
+
+    /**
+     * Override Hierarchy.map for InterfaceItem to draw them.
+     * @param func ignored parameter
+    */
+    void Hierarchy<InterfaceItem>::map(void (*func)(InterfaceItem*) = nullptr) {
+            _ref->draw();
+            for(auto child : _children) {
+                child->map(nullptr);
+        }
+    }
 }
 
 #endif // !INTERFACE_ITEM_HPP
